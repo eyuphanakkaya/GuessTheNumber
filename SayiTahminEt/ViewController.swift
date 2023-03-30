ca193c44843eb76da063a90c67ddd68ad887a606
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-03-30 10:27:49 +0300

    Initial Commit

---
+++ SayiTahminEt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayiTahminEt
+//
+//  Created by Eyüphan Akkaya on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
